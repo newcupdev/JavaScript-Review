1a654acd41341fe3c500ae7f04c763ce40932e90
Angelo <acnucup0610@gmail.com>
2021-06-02 21:10:11 -0700

    Update script.js
    Data Types Lecture

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -15,11 +15,14 @@ Rules
 
 /** Assignment 1
 
+PROBLEM 
+
 1. Declare variables called 'country', 'continent' and 'population' and assign
    their values according to your own country (population in millions)
 2. Log their values to the console    
 
- */
+ 
+SOLUTIOn
 
 let country = 'Philippines';
 let continent = "Asia";
@@ -29,3 +32,23 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
+*/
+
+/** DATA TYPES */
+
+/**
+7 Primitive Data Types
+
+1. Number: Floating point numbers -> used for decimals and integers
+2. String: Sequence of characters -> used for text
+3. Boolean: Logical type that can only be TRUE or FALSE -> use for taking decisions
+
+4. Undefined: Value taken by a variable that is not yet defined('empty value')
+5. Null: Also means 'empty value'
+6. Symbol (ES2015): Value that is unique and cannot be changed [Not useful for now]
+7. BigInt (ES2020): Larger integers than the Number type can hold
+
+JavaScript has dynamic typing: We don NOT have to manually define the data type of
+the value store in a variable, Instead, data types are determing AUTOMATICALLY
+
+ */
