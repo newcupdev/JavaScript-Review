c0521614248869deaa765ad01bdf98e3c5c21070
Angelo <acnucup0610@gmail.com>
2021-06-02 20:54:07 -0700

    Update script.js
    Variables and Values lecture
    Variables and Values assignment

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,31 @@
+/** VALUES AND VARIABLES */
+
+/** Conventions for naming variables 
+
+camelCase - first word as lowercase then subsequent words has uppercase first letter
+            e.g firstName, firstNamePerson
+
+Rules
+- variables can't start with a number
+- variables can contain numbers, letters, underscore or the dollar sign
+- variables can't use reserve keywords e.g this, function
+- variables can't start with uppercase
+
+**/
+
+/** Assignment 1
+
+1. Declare variables called 'country', 'continent' and 'population' and assign
+   their values according to your own country (population in millions)
+2. Log their values to the console    
+
+ */
+
+let country = 'Philippines';
+let continent = "Asia";
+let population = 110914345; 
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
