80ef967f33dd8d9fbcbd348b675cefe71ac0b2a8
Angelo <acnucup0610@gmail.com>
2021-06-09 21:09:45 -0700

    Update script.js
    Added If/else assignment and solution

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -319,4 +319,26 @@ if (birthYear <= 2000) {
 }
 console.log(century);
 
+// Assignment
+1. If your country's population is greater than 33 million, log a string like this to the console: 
+"Portugal's population is above average." Otherwise, log a string like "Portugal's population is 22 million 
+below average" 
+2. After checking the result, change the population temporarily to 13 and then to 130. See the different results,
+and set the population back to original
+
+// Solution
  */
+
+let country = 'Philippines';
+let continent = 'Asia';
+
+let population = 13; 
+let average = 33;
+let language = 'tagalog';
+
+if (population > 33) {
+   console.log(`${country}'s population is above average.`);
+} else {
+   console.log(`${country}'s population is ${average - population} million below average.`)
+   
+}
